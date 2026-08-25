b2f3baf746e7cc13dfebafffc438227127f626f1
ckyoon1110 <smk4397@gmail.com>
2026-08-25 23:55:21 +0900

    .

---
+++ Views/NodeGraphPreviewWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace ImageGen.Views;
+
+public partial class NodeGraphPreviewWindow : Window
+{
+    public NodeGraphPreviewWindow()
+    {
+        InitializeComponent();
+    }
+}
